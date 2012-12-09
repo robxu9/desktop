2f62490debd81940dc30e8da89cb257d6d7f77df
Alexey Kuznetsov <axet@icloud.com>
2012-12-09 17:24:47 +0100

    update merge imports

--- src/main/java/com/github/axet/desktop/os/mac/OSX.java
+++ src/main/java/com/github/axet/desktop/os/mac/OSX.java
@@ -3,11 +3,10 @@ package com.github.axet.desktop.os.mac;
 import java.io.File;
 import java.lang.reflect.Method;
 
-import com.github.axet.apple.CFArrayRef;
-import com.github.axet.apple.CFStringRef;
 import com.github.axet.desktop.Desktop;
-
-import fundations.NSFileNanager;
+import com.github.axet.desktop.apple.CFArrayRef;
+import com.github.axet.desktop.apple.CFStringRef;
+import com.github.axet.desktop.apple.fundations.NSFileNanager;
 
 public class OSX extends Desktop {
 
