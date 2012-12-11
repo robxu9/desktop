e5f250f67639f06297237e558453cbdd38fa45e5
Alexey Kuznetsov <axet@icloud.com>
2012-12-11 14:23:45 +0100

    update tests

--- src/test/java/com/github/axet/desktop/DesktopTest.java
+++ src/test/java/com/github/axet/desktop/DesktopTest.java
@@ -1,13 +1,13 @@
-package com.github.axet.desktop;
-
-public class DesktopTest {
-    public static void main(String[] args) {
-        DesktopFolders d = Desktop.getDesktopFolders();
-
-        System.out.println("Home: " + d.getHome());
-        System.out.println("Documents: " + d.getDocuments());
-        System.out.println("AppFolder: " + d.getAppData());
-        System.out.println("Desktop: " + d.getDesktop());
-        System.out.println("Downloads: " + d.getDownloads());
+    package com.github.axet.desktop;
+    
+    public class DesktopTest {
+        public static void main(String[] args) {
+            DesktopFolders d = Desktop.getDesktopFolders();
+    
+            System.out.println("Home: " + d.getHome());
+            System.out.println("Documents: " + d.getDocuments());
+            System.out.println("AppFolder: " + d.getAppData());
+            System.out.println("Desktop: " + d.getDesktop());
+            System.out.println("Downloads: " + d.getDownloads());
+        }
     }
-}
---
+++ src/test/java/com/github/axet/desktop/SysTrayTest.java
@@ -0,0 +1,58 @@
+    package com.github.axet.desktop;
+    
+    import java.awt.event.ActionEvent;
+    import java.awt.event.ActionListener;
+    
+    import javax.swing.JFrame;
+    import javax.swing.JMenuItem;
+    import javax.swing.JPopupMenu;
+    
+    public class SysTrayTest extends JFrame {
+    
+        DesktopSysTray sys = Desktop.getDesktopSysTray();
+        JPopupMenu menu;
+    
+        DesktopSysTray.Listener ml = new DesktopSysTray.Listener() {
+            @Override
+            public void mouseLeftClick() {
+                System.out.println("left click");
+            }
+    
+            @Override
+            public void mouseLeftDoubleClick() {
+                System.out.println("double click");
+            }
+    
+            @Override
+            public void mouseRightClick() {
+                System.out.println("right click");
+                sys.showContextMenu();
+            }
+    
+        };
+    
+        public SysTrayTest() {
+            super("MainFrame");
+    
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+            menu = new JPopupMenu();
+            JMenuItem menuItem1 = new JMenuItem("test1");
+            menuItem1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    System.out.println("test1");
+                }
+            });
+            menu.addSeparator();
+    
+            sys.addListener(ml);
+            sys.setTitle("Java tool2");
+            sys.setMenu(menu);
+            sys.show();
+        }
+    
+        public static void main(String[] args) {
+            new SysTrayTest();
+        }
+    }
